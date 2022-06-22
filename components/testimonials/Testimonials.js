ae8c0798f4759150a780b074150ea91911b2a485
eds2002 <es23jr@gmail.com>
2022-06-22 16:07:35 -0400

    testimonials change

--- components/testimonials/Testimonials.js
+++ components/testimonials/Testimonials.js
@@ -24,14 +24,14 @@ const Testimonials = () => {
         <div className="relative max-w-2xl px-4 mx-auto sm:px-6 lg:max-w-7xl lg:px-8">
             <div className = "flex flex-col items-center justify-center">
                 <h1 className = "text-3xl font-bold text-center text-white sm:text-4xl">
-                    We're the best - for good reasons
+                    We&apos;re the best - for good reasons
                 </h1>
             </div>
             <div className = "flex flex-col w-full md:flex-row ">
                 {UserTestimonials.map((review,id)=>(
                     <div className = "flex flex-col items-center justify-center w-full px-4 mx-auto my-10 sm:px-6 lg:max-w-7xl lg:px-8" key = {id}>
                         <div className = "flex flex-col items-center justify-center mt-10">
-                            <p className = "mt-5 text-center text-gray-200 text-md">"{review.review}"</p>
+                            <p className = "mt-5 text-center text-gray-200 text-md">&quot;{review.review}&quot;</p>
                         </div>
                         <h1 className = "mt-5 text-sm italic font-medium text-gray-500">- {review.name}</h1>
                         <p className = "text-xs text-gray-500">{review.location}</p>
--- pages/404.js
+++ pages/404.js
@@ -7,7 +7,7 @@ export default function error(){
     <div className="relative flex items-center justify-center w-full h-full max-w-2xl mx-auto lg:max-w-7xl lg:px-8">
         <div className = "w-full h-full px-4 text-white">
           <div className = "h-[100vh] text-white text-5xl flex items-center justify-center flex-col font-bold w-full py-24">
-            <Image src = {errorImg} priority/>
+            <Image src = {errorImg} priority alt = "Nothing to see here, move along. "/>
             <p className = "text-8xl">404</p>
             <p className = "text-2xl font-normal">Oops. Something went wrong</p>
             <Link href = "/">
